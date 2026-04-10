ad0ebc3cc4e659565bd1de0f9d8792d95e0a90b4
Louis W <phineco@qq.com>
2026-04-10 14:17:16 +0800

    加入百度访问统计功能

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,6 +1,7 @@
 import Navigation from '@/components/Navigation';
 import FloatingWhatsApp from '@/components/FloatingWhatsApp';
 import { getDictionary } from '@/i18n/dictionaries';
+import Script from 'next/script';
 
 export default async function LocaleLayout({
   children,
@@ -14,6 +15,19 @@ export default async function LocaleLayout({
 
   return (
     <html lang={locale}>
+      <head>
+        <Script id="baidu-analytics" strategy="afterInteractive">
+          {`
+            var _hmt = _hmt || [];
+            (function() {
+              var hm = document.createElement("script");
+              hm.src = "https://hm.baidu.com/hm.js?a4df11e3f011a97fabbbd3b76d9c373e";
+              var s = document.getElementsByTagName("script")[0];
+              s.parentNode.insertBefore(hm, s);
+            })();
+          `}
+        </Script>
+      </head>
       <body className="bg-gray-50 text-gray-900 antialiased min-h-screen">
         <Navigation locale={locale} dict={dict.Navigation} />
         {children}
